92edf32aeb82def4932a8b63f9f9df0ff9ced6d8
letikayo <wakawashi2030@gmail.com>
2021-10-04 16:39:22 -0300

    fiz os exercicios de variaveis no arquivo javascript. js

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,108 @@
+//Exercicios de Interpretação de Códigos
+
+//Exercicio 1
+// let a = 10
+// let b = 10
+
+// console.log(b)
+
+// b = 5
+// console.log(a, b)
+
+//resposta exercicio1=  10 e 10, 5
+
+//Exercicio 2
+
+// let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c)
+
+//resposta exercicio2 = 10, 10 e 10
+
+//Exercicio 3
+
+// let p = prompt("Quantas horas você trabalha por dia?")
+// let t = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${t/p} por hora`)
+
+//resposta exercicio3 =  horasTrabalhadasDia e valorRecebidoDia
+
+//Exercicio de escrita de código
+
+//Exercicio 1
+
+// a) Declare uma variável para armazenar um nome, sem atribuir um valor.
+
+// b) Declare uma variável para armazenar uma idade, sem atribuir um valor.
+
+// c) Imprima na tela o tipo dessas variáveis que acabou de criar, usando o comando `typeof`.
+
+// d) Reflita: por que esse tipo foi impresso? Escreva a resposta em um comentário de código.
+
+// resposta a  const nomePessoa = 
+// resposta b  let idadePessoa = 
+// resposta c  console.log (typeof nomePessoa, typeof idadePessoa)
+// resposta d  não tem um valor definido, undefined
+
+//e) Pergunte ao usuário seu nome e sua idade, atribuindo esses dois valores às variáveis que acabou de criar.
+
+// Exercicio 2
+
+//Escreva um programa que faça 3 perguntas de Sim ou Não, armazenado em uma variável. 
+//Por exemplo: "Você está usando uma roupa azul hoje?". Depois, siga os passos:
+
+// a) Crie três novas variáveis, contendo as respostas
+
+
+// const pergunta1 = prompt ("Você está usando uma roupa azul hoje?")
+// const pergunta2 = prompt ("Você está cansada?")
+// const pergunta3 = prompt ("Você gosta de cachorros?")
+
+// let resposta1 = pergunta1
+// let resposta2 = pergunta2
+// let resposta3 = pergunta3
+
+// console.log("Você está usando uma roupa azul hoje?", "-", resposta1)
+// console.log("Você está cansada?", "-", resposta2)
+// console.log("Você gosta de cachorros?", "-", resposta3)
+
+
+//Exercicio 3
+
+// Crie a lógica que deve ser inserida no código para que os valores de a e b sejam trocados, 
+// independente de qual valor essas variáveis assumam inicialmente (ou seja: não basta dizer que 
+// a = 25 e b = 10 porque se os valores iniciais mudarem, a lógica do seu programa teria que mudar também). 
+
+// let a = 10
+// let b = 25
+// let c
+
+// c=a 
+// //valor 10
+
+// a=b
+// //valor 25
+
+// b = c 
+// //valor 10
+
+// console.log ("a", a)
+// console.log ("b", b)
+
+// Faça um programa que receba dois números do usuário e faça as seguintes operações,
+// imprimindo os resultados no console da seguinte forma:
+//1. O primeiro número somado ao segundo número resulta em: x.
+//2. O primeiro número multiplicado pelo segundo número resulta em: y.
+
+//Desafio
+
+// primeiroNumero = prompt("Digite o primeiro numero")
+// segundoNumero = prompt("Digite o segundo numero")
+
+// console.log(Number(primeiroNumero) + Number(segundoNumero))
+
+// console.log(Number(primeiroNumero) * Number(segundoNumero))
