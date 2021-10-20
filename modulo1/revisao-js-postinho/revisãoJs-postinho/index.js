cb8a13e107f9c3473f3aaf3fd76ab591980df646
letikayo <wakawashi2030@gmail.com>
2021-10-20 14:42:47 -0300

    Atualizado exercicicio 3, feito o 4

--- modulo1/revisao-js-postinho/revisãoJs-postinho/index.js
+++ modulo1/revisao-js-postinho/revisãoJs-postinho/index.js
@@ -19,7 +19,7 @@
 // }
 // console.log(checarIgualdade(1, 2));
 
-// // // c-)Faça uma função chamada "verificaSeEMaior"
+//  c-)Faça uma função chamada "verificaSeEMaior"
 
 // function verificaSeEMaior(num1,num2){
 //     if(num1 > num2){
@@ -50,46 +50,85 @@
 
 // // Exercício 3------------------------------------------------------------------------------------
 
-const cadastro = () => {
+// const cadastro = () => {
    
-    const usuario = []
-    const nomeDoUsuario = prompt("Digite o seu nome?")
-    const anoDeNascimento = Number("Digite seu ano de nascimento?")
-    const senhaDoUsuario = prompt("Digite a sua senha, ela deve ter mais de seis digitos")
-    const nacionalidadeUsuario = prompt("Digite a sua nacionalidade?")
+//     const usuario = []
+//     const nomeDoUsuario = prompt("Digite o seu nome?")
+//     const anoDeNascimento = Number(prompt("Digite seu ano de nascimento?"))
+//     const senhaDoUsuario = prompt("Digite a sua senha, ela deve ter mais de seis digitos")
+//     const nacionalidadeUsuario = prompt("Digite a sua nacionalidade?")
 
-    const idadeAtual = 2021 - anoDeNascimento
+//     const idadeAtual = 2021 - anoDeNascimento
 
-    // if(idadeAtual>=18 ===        === "brasileira")
+//     // if(idadeAtual>=18 ===        === "brasileira")
 
-    if(idadeAtual>18 && senhaDoUsuario.length >=6 && nacionalidade === "brasileira" ){
+//     if(idadeAtual >= 18 && senhaDoUsuario.length >=6 && nacionalidadeUsuario === "brasileira" ){
 
-        console.log ("usuário cadastrado com sucesso")
+//         console.log (`Olá ${nomeDoUsuario}, usuário cadastrado com sucesso`)
 
-    }else{
+//     }else{
 
-        console.log("Não foi possivel cadastrar o usuário")
-    }
-}
-
-//     //  Sua lógica aqui
-
+//         console.log("Não foi possivel cadastrar o usuário")
+//     }
 // }
-console.log(cadastro());
+
+// Sua lógica aqui
+
+
 
 // // Exercício 4-----------------------------------------------------------------------------------------------
 
+// Escreva uma função que peça uma senha para o usuário e verifique se está é a mesma senha 
+// cadastrada por ele.  Dentro da função defina uma senha válida  Ex: const senha=”labenu”
+
+// - Caso a senha seja válida imprima no console a mensagem (Usuário Logado)
+// - Caso a senha seja inválida imprima no console (Senha Inválida)
+
 // const login = () => {
 //     const login = "labenu"
-//     //  Sua lógica aqui
+
+// function login (){
+// const senha = prompt("Digite a sua senha cadastrada no site. Ela deve ter mais de seis digitos")
+
+// if(senha === "Labenu"){
+//     console.log ("Usuário logado")
+
+// }else{
+//     console.log ("Senha Inválida")
+
+// }
 
 // }
 
 // console.log(login());
 
+
 // // Exercício 5----------------------------------------------------------------------------------------------------
 
+// Escreva uma função que receba do usuário seu nome, e o nome da vacina que tomou. 
+// E retorne a mensagens a seguir dependendo da vacina tomada.   
+
+
+// `Olá ${nome}! A próxima dose da ${vacina} é daqui a ${tempo} dias. Compareça no posto na data ${data}.`
+// Sabendo que as vacinas tem tempos diferentes  para a aplicação da segunda dose crie uma única variável 
+// ‘tempo” que mude 
+// seu valor dependendo da vacina informada pelo usuário, faça o mesmo com “data”.
+
 // const primeiraDose = () => {
+
+//     const nomeDoUsuario = prompt("Digite seu nome:")
+//     const vacinaPrimeiraDose = prompt("Digite o nome da vacina que tomou:")
+//     const dataVacina = Number(prompt("Digite a data que você tomou a primeira dose da vacina:")
+//     const dataAtual = Number(prompt("Digite a data de hoje:")
+
+//     let tempoIntervaloVacinas = dataAtual - dataVacina
+
+
+//     if (vacinaPrimeiraDose === Corona &&          )
+//     //corona = 28 dias
+//     //Astrazeneca = 90 dias
+//     // Pfizer = 90 dias
+
 
 //     //  Sua lógica aqui
 
