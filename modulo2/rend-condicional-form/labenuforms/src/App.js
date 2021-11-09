1c97503cc44611462a7eb5d2307bbe393663a663
Leticia Silva <wakawashi2030@gmail.com>
2021-11-09 15:57:09 -0300

    atualizei o arquivo
    retirei o styled

--- modulo2/rend-condicional-form/labenuforms/src/App.js
+++ modulo2/rend-condicional-form/labenuforms/src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import styled from 'styled-components'
+
 import Etapa1 from './components/Etapa1'
 import Etapa2 from './components/Etapa2'
 import Etapa3 from './components/Etapa3'
