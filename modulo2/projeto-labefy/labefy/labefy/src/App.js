161225ab35b2a417cbdb12f556216cb041d924a8
Leticia Silva <wakawashi2030@gmail.com>
2021-11-19 17:23:45 -0300

    Atualizando o labefy
    montando components e telas

--- modulo2/projeto-labefy/labefy/labefy/src/App.js
+++ modulo2/projeto-labefy/labefy/labefy/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TelaCriarPlayList from './components/TelaCriarPlayList';
+import TelaListaPlayList from './components/TelaListaPlayList';
 
-function App() {
+export default class App extends React.Component {
+  state ={
+      telaAtual: "Criar minha PlayList"
+
+  }
+  
+  escolheTela = () =>{
+      switch(this.state.telaAtual){
+          case "Criar minha PlayList":
+            return <TelaCriarPlayList irParaListaPlayList={this.irParaListaPlayList}/>
+          case "Lista PlayList":
+            return<TelaListaPlayList irParaCriarPlayList={this.irParaCriarPlayList}/>
+          default:
+            return <div>Erro! Pagina not found...</div>
+      }
+
+
+
+  }
+  irParaCriarPlayList = () => {
+      this.setState({telaAtual: "Criar minha PlayList"})
+
+  }
+
+  irParaListaPlayList = () => {
+      this.setState({telaAtual:"Lista PlayList"})
+
+  }
+
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div>
+          {this.escolheTela()}         
+
+        </div>
+  )
+}
 }
 
-export default App;
