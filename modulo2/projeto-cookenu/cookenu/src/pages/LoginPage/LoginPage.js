b0300bcee3c56a0ffc93ce3f141ec968e1de6538
Leticia Silva <wakawashi2030@gmail.com>
2021-12-16 18:18:19 -0300

    atualização do codigo page login
    atualização dos comandos

--- modulo2/projeto-cookenu/cookenu/src/pages/LoginPage/LoginPage.js
+++ modulo2/projeto-cookenu/cookenu/src/pages/LoginPage/LoginPage.js
@@ -1,14 +1,59 @@
 import React from 'react'
+import {LogoImage, ScreenContainer, InputContainer} from '../LoginPage/styled-components'
+import logo from '../../assets/logo.png'
+import useForm from '../../hooks/useForm'
+import Textfield from '@material-ui/core/TextField'
+import { Button } from '@material-ui/core'
 
 const LoginPage = () => {
+    const [form, onChange, clear] = useForm ({email:"", password:""})
+
+    const onSubmitForm = () =>{
+
+    }
+    
+    
 
     return (
-        <div>
+        <ScreenContainer>
+            <LogoImage src={logo}/>
 
-            <h1>LoginPage</h1>
+        <InputContainer>
+            <form onSubmit={onSubmitForm}>
+                <Textfield
+                    name = {"email"}
+                    value = {form.email}
+                    onChange = {onChange}
+                    label = {"E-mail"} 
+                    variant = {"outlined"}
+                    fullWidth
+                    margin = {"normal"}                     
+                />
 
-        </div>
+                <Textfield
+                    name = {"password"}
+                    value = {form.password}
+                    onChange = {onChange} 
+                    label = {"Senha"} 
+                    variant = {"outlined"}   
+                    fullWidth
+                    margin = {"normal"}                     
+                />
 
+                <Button
+                    fullWidth
+                    variant = {"contained"}
+                    color = {"primary"}
+                >
+                    Fazer Login
+                </Button>
+
+
+
+            </form>
+        </InputContainer>
+
+        </ScreenContainer>
 
     )
 
