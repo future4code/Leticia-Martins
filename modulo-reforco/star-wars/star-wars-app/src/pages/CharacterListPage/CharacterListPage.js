98a17b121398f393c57f941f55f44e163ad81c2c
Leticia Silva <wakawashi2030@gmail.com>
2022-01-09 18:41:45 -0300

    Parte 3 - lista de personagens
    lista de personagens, requisição da api funcionando e inicio do styled

--- modulo-reforco/star-wars/star-wars-app/src/pages/CharacterListPage/CharacterListPage.js
+++ modulo-reforco/star-wars/star-wars-app/src/pages/CharacterListPage/CharacterListPage.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import axios from 'axios'
+import { BASE_URL } from '../../constants/urls'
+import { CharacterCard } from './styled'
 
 export default class CharacterListPage extends React.Component{
+    state = {
+        charactersList: []
+    }
+
+    componentDidMount(){
+        this.getCharacterList()
+    }
+    getCharacterList = () => {
+        axios.get(`${BASE_URL}/people`)
+        .then((res) => this.setState({charactersList: res.data.results}))
+        .catch((err) => console.log (err.response))
+    }
+
     render(){
-        return <div>Lista</div>
+        const characters=this.state.charactersList.map((person)=>{
+            return <CharacterCard key= {person.url}>{person.name}</CharacterCard>
+    }
+        )
+
+        console.log (this.state.charactersList)
+        return <div>{characters}</div>
     }
 }
