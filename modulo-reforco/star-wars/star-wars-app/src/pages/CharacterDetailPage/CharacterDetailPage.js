756cdd238b72ff19eb1f5cee05dd0f5e99a00386
Leticia Silva <wakawashi2030@gmail.com>
2022-01-16 21:14:13 -0300

    Atualização

--- modulo-reforco/star-wars/star-wars-app/src/pages/CharacterDetailPage/CharacterDetailPage.js
+++ modulo-reforco/star-wars/star-wars-app/src/pages/CharacterDetailPage/CharacterDetailPage.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { goToListPage } from '../../routes/coordinator'
+import {useHistory} from 'react-router-dom'
 
 const CharacterDetailPage = ()=> {
+    const history = useHistory()
     
-        return <div>Detalhe</div>
+        return (<div>
+            <h1>Detalhe de personagens</h1>
+            <button> onClick = {()=>goToListPage(history)} </button>
+            
+            </div>)
     }
 
     export default CharacterDetailPage
